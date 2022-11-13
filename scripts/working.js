32b417533b24ace01f18e84c383b29fb47b089d5
Charonn <charonn.edwards412@gmail.com>
2022-11-13 02:29:49 -0500

    wrote function to get input value of the dropdown menu on searchPage.html

--- scripts/working.js
+++ scripts/working.js
@@ -8,5 +8,14 @@ const mountainDropDown = document.getElementById('mountainNames');
 for(let i = 0; i < length; i++) {
     let mountains = document.createElement('option');
     mountains.textContent = mountainsArray[i].name;
+    mountains.value = mountainsArray[i].name;
     mountainDropDown.appendChild(mountains)
 }
+
+function getValue() {
+    let inputValue = document.getElementById('mountainNames').value;
+    alert(inputValue)// lets me know that i have a hold of each input value
+}
+
+
+
