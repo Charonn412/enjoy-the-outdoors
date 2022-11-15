07bf417307b2511692d4b0512f3e88567b4835a3
Charonn <charonn.edwards412@gmail.com>
2022-11-15 10:53:47 -0500

    added functionality to info page

--- scripts/infoPage.js
+++ scripts/infoPage.js
@@ -1,26 +1,39 @@
-
 // <------------------------------------------------------Info Page--------------------------------------------------------------->
-let mountainsLength = mountainsArray.length
+let mountainsLength = mountainsArray.length;
 // for (let i = 0; i < length; i++) {
 //   let mountainNames = (mountainsArray[i].name);
 //   console.log(mountainNames);
 // }
 
-const mountainDropDown = document.getElementById('mountainNames');
-for(let i = 0; i < mountainsLength; i++) {
-    let mountains = document.createElement('option');
-    mountains.textContent = mountainsArray[i].name;
-    mountains.value = mountainsArray[i].name;
-    mountainDropDown.appendChild(mountains)
+const mountainDropDown = document.getElementById("mountainNames");
+for (let i = 0; i < mountainsLength; i++) {
+  let mountains = document.createElement("option");
+  mountains.textContent = mountainsArray[i].name;
+  mountains.value = mountainsArray[i].name;
+  mountainDropDown.appendChild(mountains);
 }
 
 function getValue() {
-    let inputValue = document.getElementById('mountainNames').value;
-    alert(inputValue)// lets me know that i have a hold of each input value
+  let inputValue = document.getElementById("mountainNames").value;
+  alert(inputValue); // lets me know that i have a hold of each input value
 }
 
+let infoSearchResults = [];
 
-
-
-
-
+ function arrayInfoElements(mountains) {
+   return `<li>${mountains.name}</li> <li>${mountains.elevation}</li> <li>${mountains.effort}</li> <li>${mountains.img}</li> <li>${mountains.desc}</li> <li>${mountains.coords.lat.lng}</li>  `;
+ }
+  
+document.onchange = function clickAndLoadInfo() {
+  infoSearchResults = [];
+  for (let i = 0; i < mountainsLength; i++) {
+    let infoValue = document.getElementById("mountainNames").value;
+    if (mountainsArray[i].name == infoValue) {
+      infoSearchResults.push(mountainsArray[i]);
+      console.log(infoValue);
+             console.log(infoValue.length);
+             document.getElementById("showArray").innerHTML =
+               infoSearchResults.map(arrayInfoElements);
+    }
+  }
+};
--- scripts/search.js
+++ scripts/search.js
@@ -15,23 +15,29 @@ function collectValue() {
   alert(inputValue);
 }
 
+const searchList = document.getElementById("searchList");
+
 let searchResults = [];
 
+function arrayElements(parks) {
+  return ` <li> ${parks.LocationName} || ${parks.Address} || ${parks.City} || ${parks.State} || ${parks.ZipCode} || ${parks.Phone} || ${parks.Latitude} || ${parks.Longitude} <li>  +`;
+}
+
 document.onchange = function clickAndLoad() {
-    searchResults = [];
+  searchResults = [];
   for (let i = 0; i < nationalParksLength; i++) {
     let inputValue = document.getElementById("location").value;
     if (nationalParksArray[i].State == inputValue) {
-      searchResults.push(nationalParksArray[i]); 
-      console.log(searchResults)
-      console.log(searchResults.length)
-      
+      searchResults.push(nationalParksArray[i]);
+      console.log(searchResults);
+      console.log(searchResults.length);
+      document.getElementById("printArray").innerHTML =
+        searchResults.map(arrayElements);
     }
   }
 };
 
 //searchResults = [];
-
 
 //trying to find utah so i can see whether or not i can find a property in an object with a find(); doesnt work
 // let results = nationalParksArray.State.find(function(element) {
@@ -57,3 +63,32 @@ function seeValue() {
   let inputValue = document.getElementById("parkType").value;
   alert(inputValue);
 }
+
+let searchResultsParkType = [];
+
+function arrayElementsParksType(parksType) {
+  return ` <li> ${parksType.LocationName} <li>  +`;
+}
+
+// document.onchange = function clickAndLoadParkType() {
+//   searchResultsParkType = [];
+//   let similarSearch = nationalParksArray.filter((arrayValue) =>
+//   arrayValue.nationalParksArray(inputValueParksType));
+//   for (let i = 0; i < nationalParksLength; i++) {
+//     let inputValueParksType = document.getElementById("parkType").value;
+
+//     if (similarSearch) {
+//       searchResultsParkType.push(nationalParksArray[i]);
+//       console.log(searchResultsParkType);
+//       console.log(searchResultsParkType.length);
+//       document.getElementById("printParkTypeArray").innerHTML =
+//         searchResultsParkType.map(arrayElementsParksType);
+//     }
+//   }
+// };
+
+// Which candies has "M&M" its name?
+// let mmCandies = products.filter((arrayValue) =>
+//   arrayValue.product.includes("M&M")
+// );
+// console.log(mmCandies);
